fbd435a8bf86f0b88db67968149d664098871eae
Kyle McSweeney <kyle.mcsweeney3@gmail.com>
2022-04-06 19:24:46 +1000

    added move and item commands

--- src/commands/item.js
+++ src/commands/item.js
@@ -1,10 +1,34 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Options } = require('discord.js');
+const { execute } = require('./ping');
+const fetch = require('node-fetch');
+const pokeEndpoint = 'https://pokeapi.co/api';
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('item')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pongg!');
-	},
+        .setDescription('Replies with details about the requested item')
+        .addStringOption(option =>
+            option.setName('item')
+                .setDescription('The name of the item to look up in the pokedex')
+                .setRequired(true)),
+        
+        async execute(interaction) {
+            const moveName = interaction.options.get('item')
+
+            fetch(`${pokeEndpoint}/v2/item/${moveName.value.toLowerCase()}`)
+                .then(response => response.json())
+                .then(data => {
+					
+                    interaction.reply({
+						content: `\`\`\`Effect: ${data.effect_entries[0].effect.replace(/\n/g, '')}\nCost: ${data.cost}\`\`\`Sprite: ${data.sprites.default}`
+                    })
+                })
+                .catch((err) => {
+                  console.log(err);
+                  interaction.reply({
+                    content: 'The requested move does not exist.'
+                  })
+                });
+        }
 };
--- src/commands/move.js
+++ src/commands/move.js
@@ -16,12 +16,12 @@ module.exports = {
         async execute(interaction) {
             const moveName = interaction.options.get('move')
 
-            fetch(`${pokeEndpoint}/v2/move/${moveName.value.toLowerCase()}`)
+            fetch(`${pokeEndpoint}/v2/move/${moveName.value.toLowerCase().replace(/ /g, '-')}`)
                 .then(response => response.json())
                 .then(data => {
 					var chars = {'-i':' 1','-ii':' 2','-iii':' 3','-iv': ' 4', '-v':' 5', '-vi': ' 6', '-vii': ' 7', '-viii': ' 8', '-iv': ' 9', '-x': ' 10'}
                     interaction.reply({
-						content: `\`\`\`Type: ${data.type.name.capitalize()}\nAccuracy: ${(data.accuracy == null) ? 'This attack never misses' : data.accuracy}\nPower: ${data.power}\nPP: ${data.pp}\nDamage type: ${data.damage_class.name.capitalize()}\nIntroduced in ${data.generation.name.replace(/-i|-ii|-iii|-iv|-vi|-vii|-viii|-iv|-x/g, m => chars[m])}\nEffect: ${data.effect_entries[0].effect}\`\`\`\n\n`
+						content: `\`\`\`Name: ${data.name.capitalize()}\nType: ${data.type.name.capitalize()}\nAccuracy: ${(data.accuracy == null) ? 'This attack never misses' : data.accuracy}\nPower: ${data.power}\nPP: ${data.pp}\nDamage type: ${data.damage_class.name.capitalize()}\nIntroduced in ${data.generation.name.replace(/-i|i{2}|i{3}|-iv|-vi|-vii|-viii|-iv|-x/g, m => chars[m])}\nEffect: ${data.effect_entries[0].effect}\`\`\`\n\n`
                     })
                 })
                 .catch((err) => {
